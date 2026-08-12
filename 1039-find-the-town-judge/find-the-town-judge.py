96e9b5847bb755682cbaf063d68c08635d14415a
Aatish2007 <srivastavaaatish279@gmail.com>
2026-08-12 14:44:50 +0530

    Time: 13 ms (53.18%) | Memory: 22.6 MB (53.95%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         trust_someone=0
@@ -9,5 +10,17 @@ class Solution:
             trusted_by [b] +=1
         for i in range (1,n+1):
             if trust_someone[i]==0 and trusted_by[i]==n-1:
+                return i
+        return -1
+'''
+#optimal Approach
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        score=[0]*(n+1)
+        for a,b in trust:
+            score[a]-=1
+            score[b]+=1
+        for i in range(1,n+1):
+            if score[i]==n-1:
                 return i
         return -1
