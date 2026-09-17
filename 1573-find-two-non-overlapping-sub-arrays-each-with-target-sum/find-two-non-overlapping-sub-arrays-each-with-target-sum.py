c86ba3d3f8aaa72f744ae4a5aa592549b3494e2a
Aatish2007 <srivastavaaatish279@gmail.com>
2026-09-17 23:33:28 +0530

    Time: 93 ms (90.50%) | Memory: 31.2 MB (66.48%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -6,8 +6,6 @@ class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
         n = len(arr)
         INF = float("inf")
-
-        # min_len[i] stores the min length of a valid subarray in arr[0...i]
         min_len = [INF] * n
 
         ans = INF
@@ -17,23 +15,15 @@ class Solution:
 
         for right in range(n):
             window_sum += arr[right]
-
-            # Shrink window if sum exceeds target
             while window_sum > target:
                 window_sum -= arr[left]
                 left += 1
-
-            # Found a valid subarray ending at 'right'
             if window_sum == target:
                 curr_len = right - left + 1
-
-                # If there's a valid non-overlapping subarray before 'left'
                 if left > 0 and min_len[left - 1] != INF:
                     ans = min(ans, curr_len + min_len[left - 1])
 
                 best_till_now = min(best_till_now, curr_len)
-
-            # Record the best minimum length seen so far up to index 'right'
             min_len[right] = best_till_now
 
         return ans if ans != INF else -1
