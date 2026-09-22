9b4d5f12f59cb80bb524d1ed426462717389ead5
Aatish2007 <srivastavaaatish279@gmail.com>
2026-09-22 20:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (97.30%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,4 +1,4 @@
-#BRUTEFORCE APPORACH
+'''BRUTEFORCE APPORACH
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
         temp=[]
@@ -8,4 +8,14 @@ class Solution:
         for i in range (len(temp)):
             nums[i]=temp[i]
         return len(temp)
-        
+'''
+#BETTER APPORACH USING SET()
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        set_1=set()
+        for num in nums:
+            set_1.add(num)
+        sorted_set=sorted(set_1)
+        for i in range(len(sorted_set)):
+            nums[i]=sorted_set[i]
+        return len(sorted_set)
