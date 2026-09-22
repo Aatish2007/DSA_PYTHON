2e35ad7f1eb71d2ac1ce8c7b4b406d063d328625
Aatish2007 <srivastavaaatish279@gmail.com>
2026-09-22 18:47:55 +0530

    Time: 5 ms (36.59%) | Memory: 20.6 MB (7.51%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,3 +1,4 @@
+'''
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -8,3 +9,19 @@ class Solution:
                 return [num_map[diff], i]
             num_map[num] = i
         return []
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums=sorted((num,i)for i,num in enumerate(nums))
+        i=0
+        j=len(sorted_nums)-1
+        while(i<j):
+            curr_sum=sorted_nums [i][0]+sorted_nums[j][0]
+            if curr_sum==target:
+                return[sorted_nums[i][1],sorted_nums[j][1]]
+            elif curr_sum<target:
+                i+=1
+            else:
+                j-=1
+        return[]   
