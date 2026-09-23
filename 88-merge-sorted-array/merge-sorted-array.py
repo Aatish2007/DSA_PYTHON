300bb33846d4175a8378e85892f8b7ea01df1220
Aatish2007 <srivastavaaatish279@gmail.com>
2026-09-23 19:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (40.19%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,6 +1,30 @@
 
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        
+        '''#BRUTEFORCE
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
+        
+        #BETTER APPORACH
+        temp=[]
+        i=j=0
+        while i<m and j<n:
+            if nums1[i]<=nums2[j]:
+                temp.append(nums1[i])
+                i+=1
+            else:
+                temp.append(nums2[j])
+        while i<m:
+            temp.append(nums1[i])
+            i+=1
+        while j<n:
+            temp.append(nums2[j])
+            j+=1
+        for k in range(len(temp)):
+            nums1[k]=temp[k]
+        '''
         i=m-1
         j=n-1
         k=m+n-1
